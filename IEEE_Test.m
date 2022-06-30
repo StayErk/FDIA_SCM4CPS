b920b53d7c27ff226f67c3b257b53e25d3280c75
Andrea Ercolino <edrioe@gmail.com>
2022-06-30 09:58:42 +0200

    Codice che mi hai inviato

--- IEEE_Test.m
+++ IEEE_Test.m
@@ -11,6 +11,19 @@ NMatlabBordeaux = [0.6350   0.0780   0.1840];
 
 % ============================================= %
 
+define_constants;
+
+% matlab colors and preset
+NMatlabRed      = [0.8500   0.3250   0.0980];
+NMatlabYellow   = [0.929    0.694    0.125 ];
+NMatlabBlue     = [0        0.4470   0.7410];  
+NMatlabViolet   = [0.4940   0.1840   0.5560];
+NMatlabGreen    = [0.4660   0.6740   0.1880];
+NMatlabCyan     = [0.3010   0.7450   0.9330];
+NMatlabBordeaux = [0.6350   0.0780   0.1840]; 
+
+% ============================================= %
+
 % ========= Table Paper ===============%
 Fc = 60; %frequency 60Hz
 Av =1;  % amplitude
