0d7ff91504b0e346c1c7d9ac0d2b6e51aa6a2c85
Andrea Ercolino <edrioe@gmail.com>
2022-06-30 12:30:11 +0200

    Updated

--- Prova_KF.m
+++ Prova_KF.m
@@ -4,18 +4,22 @@
 % x[n + 1] = Ax[n] + Bu[n] + Gw[n] (in the example B = G)
 %     y[n] = Cx[n] + Du[n] + Hw[n] + v[n]
 
-A = eye(2); % [1.1269, -0.4940, 0.1129; 1, 0, 0; 0, 1, 0];
+A =  [1.1269, -0.4940, 0.1129; 1, 0, 0; 0, 1, 0]; % valori paper: A = eye(2);
 
-B = [1;1];
+B = [-0.3832
+      0.5919
+      0.5191]; % valori paper: B = 0;
 
-C = [cos(2*pi*Fc*t), -sin(2*pi*Fc*t)];
+C =  [1 0 0];  % valori paper: C = [cos(), -sin()];
 
-D = zeros(size(C,1),size(B,2));
+D = 0;
 
 % Set up plant model:
 
 Ts = -1; % discrete time
-plant = ss(A, B, C, D, Ts);
+plant = ss(A, [B, B], C, D, Ts);
+
+
 
 process_noise_cov = 2.3;
 sensor_noise_cov  = 1;
@@ -28,28 +32,28 @@ sensor_noise_cov  = 1;
 
 
 
-plant.InputName = {'u'};
+plant.InputName = {'u', 'w'};
 plant.OutputName = {'yt'}; % true output of the plant
 
 measurementNoiseAdd = sumblk('y = yt + v'); % input of kalman filter y is the true outuput of the plant plus measurement noise
-inputNoiseAdd = sumblk('u = ut + w');
 
-kalman_filter.InputName  = {'ut', 'y'};
+
+kalman_filter.InputName  = {'u', 'y'};
 kalman_filter.OutputName = {'y_hat', 'x1_hat', 'x2_hat', 'x3_hat'};
 
-simulation = connect(inputNoiseAdd, plant, measurementNoiseAdd, kalman_filter, {'ut', 'w', 'v'}, {'yt', 'y_hat', 'x1_hat', 'x2_hat', 'x3_hat'});
+simulation = connect(plant, measurementNoiseAdd, kalman_filter, {'u', 'w', 'v'}, {'yt', 'y_hat', 'x1_hat', 'x2_hat', 'x3_hat'});
 
 % To simulate the filter behavior we have to generate an input.
 % use a sinusoidal input vector
 
 t = (0:100)';
-ut = sin(t/5);
+u = sin(t/5);
 
 rng(10, 'twister');
 inputNoise       = sqrt(process_noise_cov) * randn(length(t), 1);
 measurementNoise = sqrt(sensor_noise_cov) * randn(length(t),1);
 
-[response, ~, state] = lsim(simulation, [ut, inputNoise, measurementNoise]);
+[response, ~, state] = lsim(simulation, [u, inputNoise, measurementNoise]);
 
 yt    = response(:,1);
 y_hat = response(:,2);
