685d3392148834c66a09ca39b386fd00cf986a5a
Andrea Ercolino <edrioe@gmail.com>
2022-06-30 10:59:04 +0200

    Con dati  dal paper

--- Prova_KF.m
+++ Prova_KF.m
@@ -4,18 +4,18 @@
 % x[n + 1] = Ax[n] + Bu[n] + Gw[n] (in the example B = G)
 %     y[n] = Cx[n] + Du[n] + Hw[n] + v[n]
 
-A = [1.1269, -0.4940, 0.1129; 1, 0, 0; 0, 1, 0];
+A = eye(2); % [1.1269, -0.4940, 0.1129; 1, 0, 0; 0, 1, 0];
 
-B = [-0.3832; 0.5919; 0.5191];
+B = [1;1];
 
-C = [1,0,0];
+C = [cos(2*pi*Fc*t), -sin(2*pi*Fc*t)];
 
-D = 0;
+D = zeros(size(C,1),size(B,2));
 
 % Set up plant model:
 
 Ts = -1; % discrete time
-plant = ss(A, [B B], C, D, Ts);
+plant = ss(A, B, C, D, Ts);
 
 process_noise_cov = 2.3;
 sensor_noise_cov  = 1;
@@ -28,31 +28,34 @@ sensor_noise_cov  = 1;
 
 
 
-plant.InputName = {'u', 'w'};
+plant.InputName = {'u'};
 plant.OutputName = {'yt'}; % true output of the plant
 
 measurementNoiseAdd = sumblk('y = yt + v'); % input of kalman filter y is the true outuput of the plant plus measurement noise
+inputNoiseAdd = sumblk('u = ut + w');
 
-kalman_filter.InputName  = {'u', 'y'};
+kalman_filter.InputName  = {'ut', 'y'};
 kalman_filter.OutputName = {'y_hat', 'x1_hat', 'x2_hat', 'x3_hat'};
 
-simulation = connect(plant, measurementNoiseAdd, kalman_filter, {'u', 'w', 'v'}, {'yt', 'y_hat', 'x1_hat', 'x2_hat', 'x3_hat'});
+simulation = connect(inputNoiseAdd, plant, measurementNoiseAdd, kalman_filter, {'ut', 'w', 'v'}, {'yt', 'y_hat', 'x1_hat', 'x2_hat', 'x3_hat'});
 
 % To simulate the filter behavior we have to generate an input.
 % use a sinusoidal input vector
 
 t = (0:100)';
-u = sin(t/5);
+ut = sin(t/5);
 
 rng(10, 'twister');
 inputNoise       = sqrt(process_noise_cov) * randn(length(t), 1);
 measurementNoise = sqrt(sensor_noise_cov) * randn(length(t),1);
 
-response = lsim(simulation, [u, inputNoise, measurementNoise]);
+[response, ~, state] = lsim(simulation, [ut, inputNoise, measurementNoise]);
 
 yt    = response(:,1);
 y_hat = response(:,2);
 x1_hat = response(:,3);
+x2_hat = response(:,4);
+x3_hat = response(:,5);
 y     = yt + measurementNoise; 
 
 clf
@@ -64,6 +67,6 @@ subplot(312), plot(t,yt-y,'g',t,yt-y_hat,'r--'),
 xlabel('Number of Samples'), ylabel('Error')
 legend('True - measured','True - filtered')
 
-subplot(313), plot(t, u,'g',t,x1_hat,'r--'),
-xlabel('Number of Samples'), ylabel('Error')
+subplot(313), plot(t, state,'g',t,[x1_hat, x2_hat,x3_hat],'r--'),
+xlabel('Number of Samples'), ylabel('State')
 legend('True - state','True - filtered')
