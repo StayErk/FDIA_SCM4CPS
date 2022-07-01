338896f9a6c6644e71e96032407c610bc736dc49
Andrea Ercolino <edrioe@gmail.com>
2022-07-01 18:30:00 +0200

    Updated

--- funzioneBella.m
+++ funzioneBella.m
@@ -1,4 +1,4 @@
-function [filtered_sig, residue]= kalmanfilter(signal,Q_in,R_in,simulation_time)
+function [filtered_sig, residue, cov_matrix]= kalmanfilter(signal,Q_in,R_in,simulation_time)
     %this Kalman filter has been modified to follow an input siusoidal
     %"signal" with approximatley 50 Hz.
     %Q_in and R_in are tuning parameters.
@@ -15,7 +15,7 @@ function [filtered_sig, residue]= kalmanfilter(signal,Q_in,R_in,simulation_time)
     z1 = zeros(size(signal));
     z2 = zeros(size(signal));
     residue = zeros(size(signal));
-    
+    cov_matrix = zeros(200, 2);
     filtered_sig = zeros(size(signal));
     for i=1:max(size(signal))
         % x_hat(k|k-1) = A x_hat(k-1)
@@ -44,7 +44,8 @@ function [filtered_sig, residue]= kalmanfilter(signal,Q_in,R_in,simulation_time)
         
         % residuo: r(t) = y(t)-y_hat(t|t-1)
         residue(i) = signal(i)-C*x_hat_pred;
-        
+        cov_matrix(i,1) = signal(i);
+        cov_matrix(i,2) = C*x_hat_pred;
         x0=x_hat;
         p0=error_cov;
         
