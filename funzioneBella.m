09ac65d93f985a2ebb2905687ebe95107c138726
JudithMonke <108357248+JudithMonke@users.noreply.github.com>
2022-07-03 10:49:13 +0200

    funzione ancora piu bella

--- funzioneBella.m
+++ funzioneBella.m
@@ -1,6 +1,6 @@
-function [filtered_sig, residue]= kalmanfilter(signal,Q_in,R_in,simulation_time)
+function [filtered_sig, residue, cov_matrix, p0, K, state_error]= kalmanfilter(signal,Q_in,R_in,simulation_time,x)
     %this Kalman filter has been modified to follow an input siusoidal
-    %"signal" with approximatley 50 Hz.
+    %"signal" with approximatley 60 Hz.
     %Q_in and R_in are tuning parameters.
     %simulation_time is the total simulation time(match input-signal).
     %filtered_sig is the Kalman-filtered output of the signal.
@@ -8,15 +8,18 @@ function [filtered_sig, residue]= kalmanfilter(signal,Q_in,R_in,simulation_time)
     A=[1.00 0.0;0.0 1.00];
     w=2*pi*60;
     %initilize values
-    x0=[0;0];
-    p0=1*eye(2);
+    x0=[0;0];   %assume initial state = 0;
+    p0=1*eye(size(A));
     Q=Q_in;
     R=R_in;
+    K =0;
+    state_error1 = zeros(200);
+    state_error2 = zeros(200);
+    state_error = [state_error1; state_error2];
     z1 = zeros(size(signal));
     z2 = zeros(size(signal));
     residue = zeros(size(signal));
-    
-    filtered_sig = zeros(size(signal));
+    cov_matrix = zeros(200, 2);
     for i=1:max(size(signal))
         % x_hat(k|k-1) = A x_hat(k-1)
         x_hat_pred = A*x0;
@@ -41,12 +44,16 @@ function [filtered_sig, residue]= kalmanfilter(signal,Q_in,R_in,simulation_time)
 
         %recreating the sinusoid   y_hat = C x_hat
         filtered_sig(i)=C*[z1(i);z2(i)];
-        
+
         % residuo: r(t) = y(t)-y_hat(t|t-1)
         residue(i) = signal(i)-C*x_hat_pred;
-        
+        cov_matrix(i,1) = signal(i);
+        cov_matrix(i,2) = C*x_hat_pred;
+        state_error1(i) = x(1) - z1(i);
+        state_error2(i) = x(2)- z2(i);
         x0=x_hat;
-        p0=error_cov;
+        p0=error_cov;   
+        K = K;
         
     end
 end
